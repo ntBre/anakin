0f308e1ff33e709bd66e42a703c10285308ffd4c
Brent Westbrook <brentrwestbrook@gmail.com>
2023-07-21 16:25:48 -0400

    impl FF.create_pvals

--- src/forcefield.rs
+++ src/forcefield.rs
@@ -1,4 +1,4 @@
-use crate::Dvec;
+use crate::{Dmat, Dvec};
 
 #[derive(Clone)]
 pub struct FF {
@@ -9,6 +9,14 @@ pub struct FF {
     /// directly used in: Creating the force field; advanced usage, be careful.
     /// TODO taken from the config file
     pub(crate) use_pvals: bool,
+
+    /// transpose of the transformation matrix. initially empty but used later.
+    /// TODO consider option
+    tmi: Dmat,
+
+    pvals0: Dvec,
+
+    plist: Vec<String>,
 }
 
 impl FF {
@@ -16,6 +24,9 @@ impl FF {
         FF {
             np: 0,
             use_pvals: false,
+            tmi: Dmat::zeros(0, 0),
+            pvals0: Dvec::zeros(0),
+            plist: Vec::new(),
         }
     }
 
@@ -23,8 +34,31 @@ impl FF {
         todo!()
     }
 
-    pub(crate) fn create_pvals(&self, xk: Dvec) -> Dvec {
-        todo!()
+    pub(crate) fn create_pvals(&self, mvals: Dvec) -> Dvec {
+        // TODO potentially experimental self.redirect feature
+
+        // TODO potentially handle self.logarithmic_map option disabled by
+        // default
+
+        // python version is flat(np.dot(self.tmi, col(mvals)). I might have to
+        // flatten the result of the multiplication or something here
+        let mut pvals = (&self.tmi * mvals) + &self.pvals0;
+
+        const CONCERN: [&str; 3] = ["polarizability", "epsilon", "VDWT"];
+
+        for i in 0..self.np {
+            if self.plist.iter().any(|j| CONCERN.contains(&j.as_str()))
+                && pvals[i] * self.pvals0[i] < 0.0
+            {
+                pvals[i] = 0.0;
+            }
+        }
+
+        // redirect parameters (for the fusion penalty function)
+        // TODO another redirect
+        // for p in self.redirect
+
+        pvals
     }
 }
 
